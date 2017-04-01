5cb2c01042b10e3f258e2e0ff3b720a40d20e9d8
jeffrey k eliasen <jeff@jke.net>
2017-03-31 22:37:47 -1000

    include flake8 step in test instructions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='rpn-calc-engine',
-    version='0.0.1',
+    version='0.1.0',
     author='jeffrey k eliasen',
     author_email='jeff+rpn-calc-engine@jke.net',
     description='an RPN calculator engine for use in workshops',
